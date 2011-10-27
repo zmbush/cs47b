dceb30ce0faf4fc0b8caf5e82447bec5a730c9ec
Zach Bush <ZaBu.Other@gmail.com>
2011-10-26 20:20:25 -0700

    add files

---
+++ src/com/zachary/Reporting.java
@@ -0,0 +1,6 @@
+package com.zachary;
+
+public class Reporting {
+	public static final int DRAW_BOARD = 1 << 0;
+	public static final int SHOW_TRIES = 1 << 1;
+}
---
+++ src/com/zachary/Solver.java
@@ -0,0 +1,11 @@
+package com.zachary;
+
+public class Solver{
+	public static void main(String[] args){
+		for(String arg: args){
+			if(arg.startsWith("-o")){
+				
+			}
+		}
+	}
+}
